948783f64905dd6fb62ce7d6c1f85dcad64a9e6d
rickdt23 <54039601+rickdt23@users.noreply.github.com>
2020-04-13 17:07:14 -0500

    Add files via upload

--- python translate.py
+++ python translate.py
@@ -1,15 +1,41 @@
-
 import openpyxl
-
 from translate import Translator
 
+#open workbook
 wb = openpyxl.load_workbook('Python Translate.xlsx')
-ws = wb['Sheet1']
-A = ws['A1'].value
-print(A)
-translater= Translator(from_lang='English', to_lang='Spanish')
-translation= translater.translate(A)
+#label individual sheets
+ws1 = wb['Sheet1']
+ws2 = wb['Sheet2']
+
+#record cell values
+
+A1 = ws1['A1'].value
+A2 = None
+if ws1['A2'].value != 0:
+    A2 = ws1['A2'].value
+
+#print English words
+print(A1)
+if A2 != None:
+    print(A2)
+
+#translate text
+translater = Translator(from_lang='English', to_lang='Spanish')
+
+translation = translater.translate(A1)
+if A2 != None:
+    translation2 = translater.translate(A2)
+
+#save translated values
+ws2['A1'].value = translation
+if A2 != None:
+    ws2['A2'].value = translation2
+
+#print Spanish words
 print(translation)
+if A2 != None:
+    print(translation2)
 
+#save workbook
+wb.save('Python Translate.xlsx')
 
-#print(wb)
