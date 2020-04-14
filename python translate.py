9bf8686b321f129de6e320c70456c83b33e096e9
rickdt23 <54039601+rickdt23@users.noreply.github.com>
2020-04-14 16:22:28 -0500

    Revision 4
    Script runs without errors but only translates and saves column 1 values.

--- python translate.py
+++ python translate.py
@@ -3,45 +3,33 @@ from translate import Translator
 
 #open workbook
 wb = openpyxl.load_workbook('Python Translate.xlsx')
+
 #label individual sheets
 ws1 = wb['Sheet1']
 ws2 = wb['Sheet2']
 
+#start at cell A1
+row_loc = 1
+column_loc = 1
 #record cell values
 
-#method 1
-#A1 = ws1['A1'].value
-#method 2
-row = 1
-column = 1
-A1 = ws1.cell(row=row, column=column).value
+while column_loc <= 10:
+    
+    while row_loc <= 10:
 
-A2 = None
-if ws1['A2'].value != 0:
-    A2 = ws1['A2'].value
+        if ws1.cell(row = row_loc, column = column_loc).value != None:
+            text = ws1.cell(row = row_loc, column = column_loc).value
 
-#print English words
-print(A1)
-if A2 != None:
-    print(A2)
 
-#translate text
-translater = Translator(from_lang='English', to_lang='Spanish')
+            t = Translator(from_lang='English', to_lang='Spanish')
+            translation = t.translate(text)
 
-translation = translater.translate(A1)
-if A2 != None:
-    translation2 = translater.translate(A2)
+            ws2.cell(row = row_loc, column = column_loc).value = translation
+            wb.save('Python Translate.xlsx')
+        row_loc += 1
+        
+    column_loc += 1
 
-#save translated values
-ws2['A1'].value = translation
-if A2 != None:
-    ws2['A2'].value = translation2
+print("done")
 
-#print Spanish words
-print(translation)
-if A2 != None:
-    print(translation2)
-
-#save workbook
-wb.save('Python Translate.xlsx')
 
